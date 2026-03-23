6fb6eeac2662916012c72a22fe86036fa4b7a4db
NicolauUP <up201805451@up.pt>
2026-03-23 17:34:55 +0000

    Add System struct and build_hopping_chain function for 1D chain calculations

---
+++ src/hamiltonians/mpo_construction.jl
@@ -0,0 +1,37 @@
+# src/hamiltonian/mpo_construction.jl
+
+# -------------------------------------
+# 1D Chain - uniform hopping t::Float64
+# -------------------------------------
+
+function build_hopping_chain(sys::System{Float64, V, W}; cutoff=1e-10, maxdim=100) where {V,W}
+    sites = sys.sites
+    L = length(sites)
+    t = sys.t
+
+    T_R_opsum = OpSum()
+    T_L_opsum = OpSum()
+
+    for l in 1:L
+        opsum_R_temp = OpSum()
+        opsum_L_temp = OpSum()
+
+        opsum_R_temp += t, "σ+",l
+        opsum_R_temp += t, "σ-",L
+
+        for m in l+1:L
+            opsum_R_temp *= t, "σ-",m
+            opsum_L_temp *= t, "σ+",m
+
+        end
+
+        T_R_opsum += opsum_R_temp
+        T_L_opsum += opsum_L_temp
+
+    end
+
+    T_R_MPO = MPO(T_R_opsum, sites)
+    T_L_MPO = MPO(T_L_opsum, sites)
+
+return +(T_R_MPO, T_L_MPO; cutoff=cutoff, maxdim=maxdim)
+end
