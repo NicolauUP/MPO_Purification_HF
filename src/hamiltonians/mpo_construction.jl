d16ce1f8cdc054070a75884137d8c26bae008eb7
NicolauUP <up201805451@up.pt>
2026-03-23 18:03:01 +0000

    Refactor build_hopping_chain function: separate translation chain logic and improve clarity

--- src/hamiltonians/mpo_construction.jl
+++ src/hamiltonians/mpo_construction.jl
@@ -1,51 +1,9 @@
 # src/hamiltonian/mpo_construction.jl
 
 export build_hopping_chain
-# -------------------------------------
-# 1D Chain - uniform hopping t::Float64
-# -------------------------------------
 
-function build_hopping_chain(sys::System{Float64, V, W}; cutoff=1e-10, maxdim=100) where {V,W}
-    sites = sys.sites
+function _build_translation_chain(sites; cutoff,maxdim)
     L = length(sites)
-    t = sys.t
-
-    T_R_opsum = OpSum()
-    T_L_opsum = OpSum()
-
-    for l in 1:L
-        opsum_R_temp = OpSum()
-        opsum_L_temp = OpSum()
-
-        opsum_R_temp += t, "σ+",l
-        opsum_R_temp += t, "σ-",L
-
-        for m in l+1:L
-            opsum_R_temp *= t, "σ-",m
-            opsum_L_temp *= t, "σ+",m
-
-        end
-
-        T_R_opsum += opsum_R_temp
-        T_L_opsum += opsum_L_temp
-
-    end
-
-    T_R_MPO = MPO(T_R_opsum, sites)
-    T_L_MPO = MPO(T_L_opsum, sites)
-
-return +(T_R_MPO, T_L_MPO; cutoff=cutoff, maxdim=maxdim)
-end
-
-
-# -------------------------------------
-# 1D Chain - modulated hopping t::MPO 
-# -------------------------------------
-
-function build_hopping_chain(sys::System{MPO, V, W}; cutoff=1e-10, maxdim=100) where {V,W}
-    sites = sys.sites
-    L = length(sites)
-    t_mpo = sys.t
 
     T_R_opsum = OpSum()
     T_L_opsum = OpSum()
@@ -55,7 +13,7 @@ function build_hopping_chain(sys::System{MPO, V, W}; cutoff=1e-10, maxdim=100) w
         opsum_L_temp = OpSum()
 
         opsum_R_temp += 1, "σ+",l
-        opsum_R_temp += 1, "σ-",L
+        opsum_L_temp += 1, "σ-",l
 
         for m in l+1:L
             opsum_R_temp *= 1, "σ-",m
@@ -71,7 +29,36 @@ function build_hopping_chain(sys::System{MPO, V, W}; cutoff=1e-10, maxdim=100) w
     T_R_MPO = MPO(T_R_opsum, sites)
     T_L_MPO = MPO(T_L_opsum, sites)
 
-    T_R_mod = apply(t_mpo, T_R_MPO; cutoff=cutoff, maxdim=maxdim)
-    T_L_mod = apply(t_mpo, T_L_MPO; cutoff=cutoff, maxdim=maxdim)
-return +(T_R_MPO, T_L_MPO; cutoff=cutoff, maxdim=maxdim)
+return T_R_MPO, T_L_MPO
+end
+# -------------------------------------
+# 1D Chain - uniform hopping t::Float64
+# -------------------------------------
+
+function build_hopping_chain(sys::System{Float64, V, W}; cutoff=1e-10, maxdim=100) where {V,W}
+   T_R, T_L = _build_translation_chain(sys.sites; cutoff=cutoff, maxdim=maxdim)
+   return +(sys.t * T_R, sys.t * T_L; cutoff=cutoff, maxdim=maxdim)
+end
+
+
+# -------------------------------------
+# 1D Chain - modulated hopping t::MPO 
+# -------------------------------------
+
+function build_hopping_chain(sys::System{MPO, V, W}; cutoff=1e-10, maxdim=100) where {V,W}
+    T_R, T_L = _build_translation_chain(sys.sites; cutoff=cutoff, maxdim=maxdim)
+    T_R_mod = apply(sys.t, T_R; cutoff=cutoff, maxdim=maxdim)
+    T_L_mod = apply(sys.t, T_L; cutoff=cutoff, maxdim=maxdim)
+    return +(T_R_mod, T_L_mod; cutoff=cutoff, maxdim=maxdim)
+end
+
+
+
+function build_H0_chain(sys::System{T,V,Nothing}; cutoff=1e-10, maxdim=100) where {T,V}
+    return build_hopping_chain(sys; cutoff=cutoff, maxdim=maxdim)
+end
+
+function build_H0_chain(sys::System{T,V,MPO}; cutoff=1e-10, maxdim=100) where {T,V}
+    H_hop = build_hopping_chain(sys; cutoff=cutoff, maxdim=maxdim)
+    return +(H_hop,  sys.W; cutoff=cutoff, maxdim=maxdim)
 end
