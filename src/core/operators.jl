5c05d3dbde3b94b5c86178117d2f855f9312a188
NicolauUP <up201805451@up.pt>
2026-03-24 14:11:42 +0000

    Add Identity_MPO function for constructing identity MPO and create mcweeny.jl for density matrix initialization documentation

--- src/core/operators.jl
+++ src/core/operators.jl
@@ -7,3 +7,12 @@ ITensors.op(::OpName"σ-", ::SiteType"Qubit") = [0 0; 1 0]
 ITensors.op(::OpName"P+", ::SiteType"Qubit") = [0 0; 0 1]
 ITensors.op(::OpName"P-", ::SiteType"Qubit") = [1 0; 0 0]
 ITensors.op(::OpName"σz", ::SiteType"Qubit") = [1 0; 0 -1]
+
+function Identity_MPO(sites::Vector{<:Index})
+    L = length(sites)
+    os = OpSum()
+    for i in 1:L
+        os += 1, "Id", i
+    end
+    return MPO(os, sites) / L
+end
---
+++ src/purification/mcweeny.jl
@@ -0,0 +1,8 @@
+
+"""
+    construct_rho_0(N, H, Id, ϵ, maxχ, H_max, H_min, Ne)
+
+Build the initial density matrix guess by linearly mapping the
+eigenvalues of H into [0,1] with the correct electron count Ne.
+"""
+
