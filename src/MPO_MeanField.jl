52334b2f5fd912dd30e32930597f615b8de69f73
NicolauUP <up201805451@up.pt>
2026-04-15 15:12:28 +0100

    Refactor module imports: Organize and update import statements in MPO_MeanField for improved clarity and structure

--- src/MPO_MeanField.jl
+++ src/MPO_MeanField.jl
@@ -1,11 +1,20 @@
 module MPO_MeanField
 
-using ITensors
+using ITensors, ITensorMPS
+using Quantics, QuanticsTCI
+using TCIITensorConversion
+import TensorCrossInterpolation as TCI
 using LinearAlgebra
+using Printf
 
-include("core/operators.jl")
-include("core/system.jl")
-include("hamiltonians/mpo_construction.jl")
+include("../src/core/operators.jl")
+include("../src/core/system.jl")
+include("../src/hamiltonians/mpo_construction.jl")
+include("../src/utils/quantics.jl")
+include("../src/purification/mcweeny.jl")
+include("../src/tci/modulations.jl")
+include("../src/tci/density_matrix.jl")
+include("../src/hf/self_consistent.jl")
 
-
+export ModelParameters, System, extract_hartree_mpo_1d, extract_fock_mpo_1d, run_scf!, MatrixChecker, construct_rho_0, perform_purification
 end # module MPO_MeanField
